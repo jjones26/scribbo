5b20265fb83e11f74fe50ea7e7b24d1835b33302
Jacob Jones <keneast24@gmail.com>
2026-05-10 20:04:40 -0400

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- scribbo/firebase-config.js
+++ scribbo/firebase-config.js
@@ -3,13 +3,13 @@
    ========================================================= */
 
 window.SCRIBBO_FIREBASE_CONFIG = {
-  apiKey:            "AIzaSyB-mAFPp2R-Gif14Sw3cIbkxvt1INMEeF4",
-  authDomain:        "scribbo-94a83.firebaseapp.com",
-  databaseURL:       "https://scribbo-94a83-default-rtdb.firebaseio.com",
-  projectId:         "scribbo-94a83",
-  storageBucket:     "scribbo-94a83.firebasestorage.app",
-  messagingSenderId: "234410369127",
-  appId:             "1:234410369127:web:8aee9ec4667775ac9ab4c2",
+  apiKey:            "",
+  authDomain:        "-",
+  databaseURL:       "-",
+  projectId:         "",
+  storageBucket:     "",
+  messagingSenderId: "",
+  appId:             "",
 };
 
 window.SCRIBBO_FIREBASE_READY = true;
